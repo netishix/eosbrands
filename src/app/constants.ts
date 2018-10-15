75fee935bde06ea0ab0d484364ac71ffbba74f1c
Nahuel Vazquez <nahuev086@gmail.com>
2018-10-15 06:44:48 -0300

    save

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,7 @@ const Constants = {
     host: 'nodes.get-scatter.com',
     port: 443,
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    code: 'eosbrandgame',
+    code: 'eosisrocking',
     currency: {
       EOS: {
         code: 'eosio.token',
